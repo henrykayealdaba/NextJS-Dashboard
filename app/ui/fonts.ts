4a66416a3200d73ded7d582fe816679f61d209ca
Heekey <160593838+henrykayealdaba@users.noreply.github.com>
2025-04-16 23:52:17 +0800

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Montserrat } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
